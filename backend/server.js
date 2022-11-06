4e2566daccbea4f38c6196ac0771afdd31127971
Chadaporn Thaikana <thaikana2544@gmail.com>
2022-11-06 13:19:53 +0700

    setup server

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,5 @@ const app = express();
 
 const port = '3000';
 app.listen(port,()=>{
-    console.log('Server is running at http://localhost'+port
+    console.log('Server is running at http://localhost'+port);
+})
