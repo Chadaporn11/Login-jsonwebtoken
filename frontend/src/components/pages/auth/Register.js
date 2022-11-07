2b8bf5345d7a23e057eaf1a626508267f5a563bc
Chadaporn Thaikana <thaikana2544@gmail.com>
2022-11-07 20:51:09 +0700

    setup ant design toastify/ create Routes,Route

--- frontend/src/components/pages/auth/Register.js
+++ frontend/src/components/pages/auth/Register.js
@@ -2,52 +2,57 @@
 import React, { useState } from 'react';
 // function
 import { register } from '../../functions/auth';
+import { toast } from 'react-toastify';
+
 
 const Register = () => {
-    const [value, setValue] = useState({
-        username: "",
-        password: "",
-        password1: ""
-    })
+  const [value, setValue] = useState({
+    username: "",
+    password: "",
+    password1: ""
+  })
 
-    const handleChange = (e) => {
-        setValue({ ...value, [e.target.name]: e.target.value, });
-    };
+  const handleChange = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value, });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (value.password !== value.password1) {
-            alert("Passwords do not match");
-        } else {
-            register(value)
-                .then((res) => {
-                    //send backend compeletes : get response from server
-                    console.log(res);
-                }).catch((err) => {
-                    //get error from server
-                    console.log(err.response.data);
-                    alert(err.response.data);
-                });
-        }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(value);
+    if (value.password !== value.password1) {
+      toast.error("Password not match");
+    } else {
+      //code
+        register(value)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          toast.success(response.data);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          toast.error(err.response.data);
+        });
     }
+  };
 
 
 
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input type="text" name="username" onChange={handleChange}></input>
-                <label>Password</label>
-                <input type="text" name="password" onChange={handleChange}></input>
-                <label>Confirm Password</label>
-                <input type="text" name="password1" onChange={handleChange}></input>
-                <button disabled={value.password.length < 6}>Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input type="text" name="username" onChange={handleChange}></input>
+        <label>Password</label>
+        <input type="text" name="password" onChange={handleChange}></input>
+        <label>Confirm Password</label>
+        <input type="text" name="password1" onChange={handleChange}></input>
+        <button disabled={value.password.length < 6}>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Register
