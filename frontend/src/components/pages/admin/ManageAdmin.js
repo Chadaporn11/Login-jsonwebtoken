53d3bba67c14393324e577ab3d55f94baa98dcd5
Chadaporn Thaikana <thaikana2544@gmail.com>
2022-11-09 00:31:27 +0700

    remove user & mange moment

--- frontend/src/components/pages/admin/ManageAdmin.js
+++ frontend/src/components/pages/admin/ManageAdmin.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react';
 import MenubarAdmin from '../../layouts/MenubarAdmin';
 import { useSelector } from 'react-redux';
 import { Switch, Select, Tag } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import moment from 'moment/min/moment-with-locales';
 
 
 //function
-import { listUser, changeStatus, changeRole } from '../../functions/users';
+import {
+    listUser,
+    changeStatus,
+    changeRole,
+    removeUser
+} from '../../functions/users';
 
 
 const ManageAdmin = () => {
@@ -40,7 +47,7 @@ const ManageAdmin = () => {
                 console.log(err.response);
 
             });
-        
+
     };
 
     const handleOnChangeRole = (e, id) => {
@@ -61,7 +68,22 @@ const ManageAdmin = () => {
             });
     };
 
-    
+    const handleRemove = (id) => {
+        if (window.confirm("Are youe sure Delete!")) {
+            removeUser(user.token, id)
+                .then(res => {
+                    console.log(res);
+                    loadData(user.token);
+
+                }).catch(err => {
+                    console.log(err.response);
+
+                });
+
+        }
+    }
+
+
 
 
     useEffect(() => {
@@ -87,6 +109,8 @@ const ManageAdmin = () => {
                                 <th scope="col">status</th>
                                 <th scope="col">created</th>
                                 <th scope="col">updated</th>
+                                <th scope="col">action</th>
+
 
                             </tr>
                         </thead>
@@ -97,22 +121,24 @@ const ManageAdmin = () => {
                                     <td>
                                         <Select
                                             style={{ width: '100%' }}
-                                            defaultValue={item.role}
+                                            value={item.role}
                                             onChange={(e) => handleOnChangeRole(e, item._id)}>
                                             {roleData.map((item, index) =>
                                                 <Select.Option value={item} key={index}>
-                                                    { item == 'admin'
-                                                    ? <Tag color='green'>{item}</Tag> 
-                                                    : <Tag color='red'>{item}</Tag>
-                                                }
+                                                    {item == 'admin'
+                                                        ? <Tag color='green'>{item}</Tag>
+                                                        : <Tag color='red'>{item}</Tag>
+                                                    }
                                                 </Select.Option>
                                             )}
                                         </Select>
                                     </td>
                                     <td>{item.enabled}
                                         <Switch checked={item.enabled} onChange={(e) => handleOnChange(e, item._id)} /></td>
-                                    <td>{item.createdAt}</td>
-                                    <td>{item.updatedAt}</td>
+                                    <td>{moment(item.createdAt).locale('th').format('LLL')}</td>
+                                    <td>{moment(item.updatedAt).locale('th').startOf(item.updatedAt).fromNow()}</td>
+                                    <td><DeleteOutlined onClick={() => handleRemove(item._id)} /></td>
+
                                 </tr>
                             ))}
                         </tbody>
