bc50993695984479ce5d6512b2fc01f6579e7af4
Chadaporn Thaikana <thaikana2544@gmail.com>
2022-11-08 19:39:31 +0700

    create Navbar admin & install bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //Redux
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
